bb48549cc85cf35ad263a6dd8badc6711bb0be27
TheZoq2 <frans.skarman@protonmail.com>
2021-05-07 23:02:59 +0200

    Add frisbee throwing to parkgoers

--- scenes/parkgoer.gd
+++ scenes/parkgoer.gd
@@ -5,14 +5,18 @@ extends KinematicBody
 # var a = 2
 # var b = "text"
 
-var since_direction_change = 0;
+var last_frisbee = 10;
+var since_direction_change = 10;
 var rng = RandomNumberGenerator.new()
 var direction = Vector3(0, 0, 0);
+
+var frisbee_template = preload("res://scenes/frisbee.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.new_velocity()
+	self.throw_frisbee()
 
 func new_velocity():
 	since_direction_change = 0
@@ -21,14 +25,23 @@ func new_velocity():
 
 	self.direction = Vector3(cos(move_angle), 0, sin(move_angle))
 
+func throw_frisbee():
+	var frisbee = frisbee_template.instance()
+	get_tree().get_root().add_child(frisbee)
+	frisbee.global_transform.origin = self.get_node("frisbee_origin").global_transform.origin
+	self.last_frisbee = 0;
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	since_direction_change += delta
+	self.since_direction_change += delta
+	self.last_frisbee += delta
 
 	if since_direction_change > randf() * 5 + 5:
 		self.new_velocity()
 
+	if last_frisbee > randf() * 5 + 5:
+		self.throw_frisbee()
 
 	if self.move_and_collide(Vector3(self.direction * delta)):
 		pass
