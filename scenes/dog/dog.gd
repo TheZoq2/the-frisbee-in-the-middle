e688c0e61531e672aaf6046d6177cf02f6d96eb7
Axel Nordanskog <axerosh@gmail.com>
2021-05-08 14:31:21 +0200

    Have dogs ignore caught frisbees
    i.e. frisbees that a hand is about to grab.

--- scenes/dog/dog.gd
+++ scenes/dog/dog.gd
@@ -40,6 +40,8 @@ func find_closest_frisbee() -> Frisbee:
 	var closest_frisbee: Frisbee = null
 	var closest_distance: float;
 	for body in frisbees:
+		if (body as Frisbee).is_caught:
+			continue
 		var distance: float = self.global_transform.origin.distance_to(
 			body.global_transform.origin)
 		if closest_frisbee == null or distance < closest_distance:
