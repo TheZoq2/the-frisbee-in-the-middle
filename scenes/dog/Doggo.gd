7be23294029b8712414cf16586be43eff315b34e
Spiegel <antda166@student.liu.se>
2021-05-08 12:47:51 +0200

    Improved dog movement and logic for catching frisbees

--- scenes/dog/Doggo.gd
+++ scenes/dog/Doggo.gd
@@ -1,14 +1,13 @@
 extends KinematicBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 class_name Doggo
 
-var velocity:Vector3 = Vector3.ZERO
+onready var _velocity:Vector3 = Vector3.ZERO
+onready var _distance:float = 0.0
 export var gravity = -5
 export var speed = 5
+export var rotation_factor = 0.5
+export var arrive_threshold = 1.25
 
 signal dog_caught_frisbee
 
@@ -16,23 +15,22 @@ signal dog_caught_frisbee
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
 	var target = find_closest_frisbee()
 	if target:
-		look_at(target.target.global_transform.origin, Vector3.UP)
-		rotation.x = 0
-		velocity = -transform.basis.z * speed
-		if transform.origin.distance_to(target.global_transform.origin) < .5:
-			velocity = Vector3.ZERO
+		_distance = transform.origin.distance_to(target.global_transform.origin)
+		if _distance < arrive_threshold:
+			#_velocity = Vector3.ZERO
 			target.queue_free()
+			target = find_closest_frisbee()
 			emit_signal("dog_caught_frisbee")
-	velocity = move_and_slide(velocity, Vector3.UP)
+			return
+		var new_dog_transform = transform.looking_at(target.global_transform.origin, Vector3.UP)
+		transform = transform.interpolate_with(new_dog_transform, (rotation_factor * delta) / _distance)
+		rotation.x = 0
+		_velocity = -transform.basis.z * speed
+
+	_velocity = move_and_slide(_velocity, Vector3.UP)
 
 func find_closest_frisbee() -> Frisbee:
 	var frisbees = get_tree().get_nodes_in_group("frisbee")
@@ -42,10 +40,9 @@ func find_closest_frisbee() -> Frisbee:
 	var closest_frisbee: Frisbee = null
 	var closest_distance: float;
 	for body in frisbees:
-		if body is Frisbee:
-			var distance: float = self.global_transform.origin.distance_to(
-				body.global_transform.origin)
-			if closest_frisbee == null or distance < closest_distance:
-				closest_frisbee = body
-				closest_distance = distance
+		var distance: float = self.global_transform.origin.distance_to(
+			body.global_transform.origin)
+		if closest_frisbee == null or distance < closest_distance:
+			closest_frisbee = body
+			closest_distance = distance
 	return closest_frisbee
