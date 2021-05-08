87d926064a371cb95e0de03ebf950b1e517a5a29
TheZoq2 <frans.skarman@protonmail.com>
2021-05-08 14:29:00 +0200

    Throw frisbees in the player's general direction

--- scenes/frisbee.gd
+++ scenes/frisbee.gd
@@ -12,10 +12,13 @@ var outline_mesh: MeshInstance
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player: Vector3 = get_parent().player.get_position()
-	var direction = rand_range(0, PI*2)
+	var player_pos: Vector3 = get_parent().player.get_position()
+
+	var player_direction = (player_pos - self.global_transform.origin).normalized();
+	print(player_direction)
+	var direction = atan2(player_direction.z, player_direction.x) + rand_range(-PI/4, PI/4)
 	var force = 3;
-	self.apply_central_impulse(Vector3(cos(direction), 0.2, sin(direction)) * force)
+	self.apply_central_impulse(Vector3(cos(direction), 0, sin(direction)) * force)
 	self.add_torque(Vector3(0, 100, 0))
 
 	outline_mesh = self.find_node("Outline") as MeshInstance
