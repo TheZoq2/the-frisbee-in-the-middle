362e1cc17a52889fca188e56ffff761bea4598b3
chasi127 <chasi127@student.liu.se>
2021-05-08 11:49:41 +0200

    fixed small merge conflict!

--- scenes/the_park/GUI.gd
+++ scenes/the_park/GUI.gd
@@ -21,19 +21,22 @@ onready var number_label = get_node("HUD/TextureRect/Panel/HBoxContainer/NR")
 #                                                               #
 #12. optional built-in virtual _init method                     #
 #13. built-in virtual _ready method                             #
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
 
 #14. remaining built-in virtual methods                         #
 func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if $GameMenu.visible:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			#$GameMenu.set("visible", false)
-			#get_tree().set("paused", false)
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			
+			$GameMenu.set("visible", false)
+			get_tree().set("paused", false)
 		
 		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			#get_tree().set("paused", true)
-			#$GameMenu.set("visible", true)
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_tree().set("paused", true)
+			$GameMenu.set("visible", true)
 #15. public methods                                             #
 
 #16. private methods                                            #
@@ -47,3 +50,13 @@ func _on_Catcher_frisbee_caught():
 	#print("# I cought one!!! ")
 	caught_frisbees += 1
 	number_label.text = str(caught_frisbees)
+
+
+func _on_ResumeButton_pressed():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$GameMenu.set("visible", false)
+	get_tree().set("paused", false)
+
+
+func _on_QuitGameButton_pressed():
+	get_tree().quit()
