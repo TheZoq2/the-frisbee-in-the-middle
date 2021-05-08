ebc397d0eaea921cd06d1fcbd47ae70b45df7729
TheZoq2 <frans.skarman@protonmail.com>
2021-05-08 09:53:51 +0200

    Make frisbees fly in random directions

--- scenes/frisbee.gd
+++ scenes/frisbee.gd
@@ -10,7 +10,9 @@ export var gravity_compensation = 9.6;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.apply_central_impulse(Vector3(3, 0, 0))
+	var direction = rand_range(0, PI*2)
+	var force = 3;
+	self.apply_central_impulse(Vector3(cos(direction), 0, sin(direction)) * force)
 	self.add_torque(Vector3(0, 100, 0))
 
 
