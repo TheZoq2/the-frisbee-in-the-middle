700223dff9deb7808a8434375579b4e34dbe124e
TheZoq2 <frans.skarman@protonmail.com>
2021-05-09 16:21:10 +0200

    Add limited treats to VR

--- scenes/VR/controller.gd
+++ scenes/VR/controller.gd
@@ -13,7 +13,6 @@ var current_treat : Node;
 var velocity : Vector3;
 var last_position : Vector3 = Vector3(0, 0, 0);
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -32,13 +31,16 @@ func _on_catch_pressed():
 	$Area._on_vr_catch_input()
 
 func _on_treat_pressed():
-	current_treat = visual_treat.instance()
-	get_node("/root/GameState").last_scene.add_child(current_treat)
+	if get_node("/root/GameData").remaining_treats > 0:
+		current_treat = visual_treat.instance()
+		get_node("/root/GameState").last_scene.add_child(current_treat)
+		get_node("/root/GameData").remaining_treats -= 1
 
 func _on_treat_release():
-	var actual_treat = treat_prefab.instance()
-	actual_treat.global_transform = current_treat.global_transform
-	get_node("/root/GameState").last_scene.add_child(actual_treat)
-	actual_treat.apply_central_impulse(self.velocity)
-	current_treat.queue_free()
-	current_treat = null
+	if current_treat:
+		var actual_treat = treat_prefab.instance()
+		actual_treat.global_transform = current_treat.global_transform
+		get_node("/root/GameState").last_scene.add_child(actual_treat)
+		actual_treat.apply_central_impulse(self.velocity)
+		current_treat.queue_free()
+		current_treat = null
